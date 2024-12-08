af6b5d9cfc2ad74df2ba7d37c57de16fad7d9dac
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2024-12-08 09:34:00 -0300

    feat: first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int life = 100;
+    public float velocidade = 1500.2f;
+    public bool isActive = true;
+    public string nomePlayer = "example";
+
+    public GameObject chao; // lista de game objects
+    public Transform inimigo; // dados de x, y e z de um game object
+
+
+    void Attack()
+    {
+
+    }
+
+    // Start is called before the first frame update
+   // void Start()
+   // {
+        
+   // }
+
+    /// <summary>
+    /// sdsds
+    /// </summary>
+    // Update is called once per frame
+////d Update()
+   //
+   //   
+   //
+
+
+  //void Attack()
+  //{
+//
+  //}
+
+  //void Moviment ()
+   //
+
+   //
+
+  //void Hit()
+   //
+
+  //}
+}
